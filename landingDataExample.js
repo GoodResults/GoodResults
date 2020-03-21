10d5e00b9057bf9c57937981aa3f7c3e1175e368
tim.ivaikin <t.s.ivaykin@gmail.com>
2020-03-21 14:11:07 +0200

    Added other types of variations

--- landingDataExample.js
+++ landingDataExample.js
@@ -40,11 +40,39 @@ const examples = [
     bookCover: [
       './media-assets/412V6YJjGNL._SL500_.jpg',
       './media-assets/cover2.jpg',
-      './media-assets/cover3.jpg'
-    ].rand()
+      './media-assets/cover3.jpg',
+      './media-assets/Cover-bestseller-3.jpg',
+      './media-assets/cover-bestseller-5.jpg'
+    ].rand(),
+    titleHeadline: [
+      'Good Results: Art of efficiency for powerful growth',
+      'Art of efficiency for powerful growth',
+      'Good Results',
+      'Guide to Good Life'
+    ]
   }
 ]
 
 const qrtip = require('qrtip')
 
 console.log(qrtip.tip(JSON.stringify(examples[2])))
+console.log(examples[2])
+
+// For years, rumors of the Marsh Girl have haunted Barkley Cove, a quiet town on the North Carolina coast. So in late 1969, when handsome Chase Andrews is found dead, the locals immediately suspect Kya Clark, the so-called Marsh Girl. But Kya is not what they say. Sensitive and intelligent, she has survived for years alone in the marsh that she calls home, finding friends in
+/*  * 5 Stars, easily! *
+
+    WHERE THE CRAWDADS SING is a gentle yet symbolic depiction of the valiant survival of Kya Clarka reclusive young girl who has been abandoned by her parents, siblings, school system, the entire town surrounding her, and what ultimately feels like life itself.;
+
+Mother Nature has literally become Kyas caretaker, and deep in a lonely Marsh along the North Carolina coast is where Kya will not only hide, but blossom into a primal independent being, coaxed inside the loyal embrace of;
+All the Stars!!!!!
+
+Can I just say that I loved everything about this book and leave it at that!?!
+
+Where the Crawdads Sing is a story of resiliency, survival, hope, love, loss, loneliness, desperation, prejudice, determination and strength. This book goes back and forth in time to tell the story of Kya Clark a.k.a. the Marsh girl. She lives on the outskirts of town, in the Marsh, and the locals look down their noses at her, she is judged, ridiculed and bullied. But there are those who show her
+
+I'm typically skeptical of books that are hyped to high heavens and end up on every book club list for months straight, not because they aren't worthy, but because I can let my expectations get the best of me and keep me from fully enjoying a wonderful book. This book exceeded my already high expectations; it emanates a quiet power, a slow drawing in and connection of reader to book, one that I found myself able to get lost in due to the lush atmosphere and the depth of emotion. I can see now
+This is a real controversial book to be hated or to be wholeheartedly loved. I was at the team of deeply in lovers. And I know most of the good readers couldnt finish it. But if they also give it a shot to try the audiobook read by Cassandra Campbell, Im positively sure that they will love the story and all the nature parts because this is not only Marsh Girls heart-wrenching, sad, poignant, poetic story, but this is also about nature.
+
+When you listen to this book, you feel like the story is
+I couldn't put the book down, I stayed up till 11 pm last night reading it was full of so much to take in. weather it was about ...
+*/
