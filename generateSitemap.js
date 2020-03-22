dc8e9f10950c55915253c9a1bb21d054db5fe8d7
tim.ivaikin <t.s.ivaykin@gmail.com>
2020-03-22 16:45:39 +0200

    Generated sitemaps

--- generateSitemap.js
+++ generateSitemap.js
@@ -6,7 +6,7 @@ const sitemapname =
   "sitemap" + Math.round(Math.random() * 1000000000000000000000) + ".txt";
 
 // Max Google limits: 50000 URLs per sitemap, max 50 Mb sitemap, 500 sitemap indexes submitted max per site
-const LANDINGS_AMOUNT_TO_GENERATE = 100;
+const LANDINGS_AMOUNT_TO_GENERATE = 40000;
 let data = "";
 for (let i = 0; i < LANDINGS_AMOUNT_TO_GENERATE; ++i) {
   data +=
